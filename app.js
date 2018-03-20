e87d2a61d0accb5d1aafcbceae2d56e451906ab8
MiniCheese26 <kaleb.medhurst@gmail.com>
2018-03-20 22:59:00 +0000

    Update

---
+++ app.js
@@ -0,0 +1,11 @@
+particlesJS.load('particles-js', 'json/blue.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js2', 'json/green.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js3', 'json/black.json', function() {
+  console.log('callback - particles.js config loaded');
+});
